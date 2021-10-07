7fb8473892c1f3ce36dc02b6997b8dd2e7658a55
J3llyBe4n <zza0423@gmail.com>
2021-10-07 18:35:00 +0900

    chapter4 if added

---
+++ chapter4/src/ifexample/ifExample1.java
@@ -0,0 +1,15 @@
+package ifexample;
+
+public class ifExample1 {
+    public static void main(String[] args){
+        int age = 2;
+
+        if(age >= 8){
+            System.out.println("학교 다님");
+            System.out.println("test");
+        }
+        else{
+            System.out.println("학교안댕김");
+        }
+    }
+}
